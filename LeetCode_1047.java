c0225aa54c7068043d2a007d26b54806ae837053
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-07 19:21:05 -0500

    Add files via upload

---
+++ LeetCode_1047.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeDuplicates(String S) {
+        StringBuilder builder=new StringBuilder();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0; i<S.length();i++){
+            if(!stack.isEmpty()){
+                if(stack.peek()==S.charAt(i)){
+                    stack.pop();
+                }
+                else{
+                    stack.push(S.charAt(i));
+                }
+                
+            }
+            else{
+                stack.push(S.charAt(i));
+            }
+        }
+        
+      while(stack.isEmpty()==false){
+          builder.append(stack.pop());
+      }
+        return builder.reverse().toString();
+    }
+}
