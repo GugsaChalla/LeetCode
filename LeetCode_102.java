c26031ade8a2d7835af39551e46e57c33129f559
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-08 19:55:05 -0500

    Add files via upload

---
+++ LeetCode_102.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        if(root==null){return res;}
+        
+        ArrayList<Integer> temp=new ArrayList<>();
+        LinkedList<TreeNode> queue=new LinkedList<TreeNode>(); 
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode current=queue.remove();
+                temp.add(current.val);
+                if(current.left!=null){
+                    queue.add(current.left);
+                }
+                if(current.right!=null){
+                    queue.add(current.right);
+                }
+    
+               
+            }
+            res.add(temp);
+            temp=new ArrayList<>();
+        }
+        return res;
+    }
+    
+}
