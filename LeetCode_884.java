203a7b94b0032349acb7228065a925e2f9fdce38
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-16 19:06:51 -0500

    Add files via upload

---
+++ LeetCode_884.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String[] uncommonFromSentences(String A, String B) {
+        
+        String[] arrA=A.split(" ");
+        String[] arrB=B.split(" ");
+        HashMap<String,Integer>map= new HashMap<>();
+        //HashSet<String> setB = new HashSet<>();
+        for(int i=0;i<arrA.length;i++){
+            if(map.containsKey(arrA[i])){
+                map.put(arrA[i],map.get(arrA[i])+1);
+            }
+            else{
+                map.put(arrA[i],0);
+            }
+        }
+        for(int j=0;j<arrB.length;j++){
+            if(map.containsKey(arrB[j])){
+                map.put(arrB[j],map.get(arrB[j])+1);
+            }
+            else{
+                map.put(arrB[j],0);
+            }
+        }
+        int spot=0;
+        Iterator iter= map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry entry=(Map.Entry) iter.next();
+            int value=(int) entry.getValue();
+            if(value==0){
+                spot++;
+            }
+        }
+        String[] res = new String[spot];
+        spot=0;
+        Iterator iter2= map.entrySet().iterator();
+        while(iter2.hasNext()){
+            Map.Entry entry=(Map.Entry) iter2.next();
+            int values=(int) entry.getValue();
+            if(values==0){
+                res[spot]=(String) entry.getKey();
+                spot++;
+            }
+        }
+        return res;
+    }
+}
