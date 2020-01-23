3039651105d36f5ec8de22ce284e926d548e9360
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-22 19:08:05 -0500

    Add files via upload

---
+++ LeetCode_1019.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        //int n=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+        int[] arr = new int[list.size()];
+       Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<list.size();i++){
+            while(!stack.isEmpty() && list.get(i)>list.get(stack.peek())){
+                
+                    arr[stack.peek()]=list.get(i);
+                    stack.pop();
+            
+                
+                
+            }
+            
+            stack.push(i); 
+        }
+        return arr;
+        
+    }
+}
