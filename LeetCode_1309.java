ed87d3f1aac98e66ca420bc673135e826031ecd3
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-26 21:42:31 -0500

    Add files via upload

---
+++ LeetCode_1309.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String freqAlphabets(String s) {
+         StringBuilder str= new StringBuilder();
+        for (int i = 0; i < s.length();) {
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                sb.append((char)('j' + Integer.parseInt(s.substring(i, i + 2)) - 10));
+                i += 3;
+            } else {
+                sb.append((char)('a' + s.charAt(i) - '1'));
+                i++;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
