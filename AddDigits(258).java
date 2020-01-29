309c2735a3baf1f8b1117d3330976dbf0396775c
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-29 18:40:50 -0500

    Update AddDigits(258).java

--- AddDigits(258).java
+++ AddDigits(258).java
@@ -1,14 +1,14 @@
 class Solution {
     public int addDigits(int num) {
-       int sum=0;
-        while(num>0||sum>9){
-            if(sum>9&&num<1){
-                num=sum;
-                sum=0;
-            }
-           sum=sum+(num%10);
-            num=num/10;
+       if(num==0){
+            return 0;
         }
-        return sum;
-    }
+        if(num%9==0){
+           return 9;
+       }
+        
+        else{
+            return num%9;
+        }
+}
 }
