3c0475eb91ca64901bb523dd98aa8581a1350d04
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-01-30 18:34:30 -0500

    Add files via upload

---
+++ LeetCode_382.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null||head.next==null){return head;}
+        ListNode res=new ListNode(0);
+        ListNode resHead=res;
+        ListNode even = new ListNode(0);
+        ListNode evenHead=even;
+        while(head!=null){
+            res.next=head;
+            res=res.next;
+            head=head.next;
+            if(head==null){
+                even.next=null;
+                break;
+            }
+            even.next=head;
+            even=even.next;
+            head=head.next; 
+            
+            }
+        
+        res.next=evenHead.next;
+        return resHead.next; 
+        }
+    }
