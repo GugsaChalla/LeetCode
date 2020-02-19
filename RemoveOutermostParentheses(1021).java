34e874d073fa55fa8a8bd0162f89f67b29c4f5ce
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-02-19 16:31:55 -0500

    Add files via upload

---
+++ RemoveOutermostParentheses(1021).java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeOuterParentheses(String S) {
+        int bool =1;
+        int count=0;
+        StringBuilder str  = new StringBuilder();
+        for(int i=1;i<S.length();i++){
+            if(bool==0){
+                i++;
+                bool=1;
+            }
+            if(S.charAt(i)=='('){count++;}
+            if(S.charAt(i)==')'){count--;}
+           if(bool==1&&count>-1){
+               str.append(S.charAt(i));
+           }
+            else if(count<0){
+                bool=0;
+                count=0;
+            }
+            
+        }
+        return str.toString();
+    }
+}
