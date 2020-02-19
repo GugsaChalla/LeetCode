ce7bc74526ff82a8bbe4dc8b1bae9890c4789ed2
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-02-19 16:33:10 -0500

    Update RemoveOutermostParentheses(1021).java

--- RemoveOutermostParentheses(1021).java
+++ RemoveOutermostParentheses(1021).java
@@ -3,6 +3,7 @@ class Solution {
         int bool =1;
         int count=0;
         StringBuilder str  = new StringBuilder();
+        
         for(int i=1;i<S.length();i++){
             if(bool==0){
                 i++;
@@ -10,6 +11,7 @@ class Solution {
             }
             if(S.charAt(i)=='('){count++;}
             if(S.charAt(i)==')'){count--;}
+            
            if(bool==1&&count>-1){
                str.append(S.charAt(i));
            }
