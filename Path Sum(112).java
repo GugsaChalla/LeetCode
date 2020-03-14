563f697600fa46572e8ea281ada9365a3be06225
GugsaChalla <45246333+GugsaChalla@users.noreply.github.com>
2020-03-13 20:35:32 -0400

    Add files via upload

---
+++ Path Sum(112).java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null){return false;}
+        return dfs(sum,root);
+    }
+    
+public boolean dfs(int num, TreeNode current){
+        //if(current.left==null&&current.right==null){return;}
+         if(current==null){return false;}
+        if(num-current.val==0&&current.left==null&current.right==null){return true;}
+        return dfs(num-current.val, current.left) || dfs(num-current.val,current.right);
+       
+    }
+}
